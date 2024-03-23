b130f72182f0b9473dd400beb209b1dde4289473
yhsphd <yhsphd05@gmail.com>
2024-03-23 22:04:12 +0900

    [ADD] 메인 페이지 배너 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,12 @@
 export default function MainPage() {
   return (
     <>
-      <main>메인 페이지 입니다.</main>
+      <main>
+        <img src="/banner.jpg" alt="Page Banner" />
+        <div style={{ marginTop: '16px' }}>
+          &quot;베프의 연애&quot;는 서로의 친구들이 함께 두 사람의 연애 고민을 상담해 줄 수 있는 플랫폼입니다.
+        </div>
+      </main>
     </>
   );
 }
