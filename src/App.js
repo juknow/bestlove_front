97f25250d59448221d21f354a6d981b4c748959b
juknow <munjunho77@naver.com>
2024-03-23 16:41:59 +0900

    [ADD] 메인페이지 디자인 조금

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MyPage from "./pages/MyPage";
-import MainPage from "./pages/MainPage";
-import NotFound from "./pages/NotFound";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MyPage from './pages/MyPage';
+import MainPage from './pages/MainPage';
+import NotFound from './pages/NotFound';
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />}></Route>
-          <Route path="/mypage/*" element={<MyPage />}></Route>
+          <Route path='/' element={<MainPage />}></Route>
+          <Route path='/mypage/*' element={<MyPage />}></Route>
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-          <Route path="*" element={<NotFound />}></Route>
+          <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,15 @@
 export default function MainPage() {
   return (
     <>
-      <div>메인 페이지 입니다.</div>
+      <header>베프의 연애</header>
+      <div className='navbar'></div>
+      <nav>
+        <button className='navbutton'>회원가입</button>
+        <button className='navbutton'>로그인</button>
+        <button className='navbutton'>마이페이지</button>
+      </nav>
+      <div className='navbar'></div>
+      <main>메인 페이지 입니다.</main>
     </>
   );
 }
