870147e72847288a0ba8178fefb25d308e607f5b
juknow <munjunho77@naver.com>
2024-03-24 07:37:12 +0900

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -63,33 +63,6 @@ export default function LoginPage() {
           </div>
         </div>
       </div>
-      <div className="container flex-column d-flex align-items-center w-screen">
-        <h2 className="text-3xl">로그인</h2>
-
-        <div className="h-2"></div>
-
-        <form action="#" method="post">
-          <div className="mb-3">
-            <div>아이디</div>
-            <input type="text" name="username" placeholder="ID" required className="form-control" />
-          </div>
-
-          <div className="mb-3">
-            <div>비밀번호</div>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              required
-              className="form-control"
-            />
-          </div>
-
-          <button type="submit" className="btn btn-primary text-black">
-            로그인
-          </button>
-        </form>
-      </div>
     </>
   );
 }
