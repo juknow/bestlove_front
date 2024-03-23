8b2a833a566c411285a32cd5effaefff197aec49
InfDev00 <119485240+InfDev00@users.noreply.github.com>
2024-03-24 05:29:29 +0900

    Update CommentCard.jsx

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -3,7 +3,7 @@ export default function CommentCard() {
     <>
       <h3 className="text-left font-bold mb-0 ">류희철</h3>
       <br />
-      <h3 className="text-left  mb-0">댓글댓글</h3>
+      <h3 className="text-left  mb-0">좋은 의견입니다.</h3>
       <br />
     </>
   );
