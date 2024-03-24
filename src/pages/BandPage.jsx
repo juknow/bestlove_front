2cf9dab3c7551c682fc5ee96917c3ee4b8044bd4
juknow <munjunho77@naver.com>
2024-03-24 09:23:00 +0900

    asd

--- src/pages/BandPage.jsx
+++ src/pages/BandPage.jsx
@@ -2,8 +2,7 @@ import BandCard from "../components/BandCard";
 
 const BandData = [
   { id: 1, title: "첫 번째 카드", content: "첫 번째 카드 내용입니다." },
-  { id: 2, title: "두 번째 카드", content: "두 번째 카드 내용입니다." },
-  { id: 3, title: "세 번째 카드", content: "세 번째 카드 내용입니다." },
+
 ];
 
 export default function BandPage() {
--- src/pages/ReadPost.jsx
+++ src/pages/ReadPost.jsx
@@ -1,10 +1,6 @@
 import CommentCard from "../components/CommentCard";
 
-const commentData = [
-  { id: 1, title: "첫 번째 카드", content: "첫 번째 카드 내용입니다." },
-  { id: 2, title: "두 번째 카드", content: "두 번째 카드 내용입니다." },
-  { id: 3, title: "세 번째 카드", content: "세 번째 카드 내용입니다." },
-];
+const commentData = [{ id: 1, title: "첫 번째 카드", content: "첫 번째 카드 내용입니다." }];
 
 export default function ReadPost() {
   return (
